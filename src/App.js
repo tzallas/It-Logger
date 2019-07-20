022eb794c333caf14c50787aff1f22be65b8a99d
tzallas <tzallasilias@gmail.com>
2019-07-20 11:14:29 +0300

    AddBtn ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useEffect } from 'react';
-
+import React, { Fragment, useEffect } from 'react';
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import './App.css';
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
 
 const App = () => {
   useEffect(() => {
     //init materialize js
     M.AutoInit();
   })
+
   return (
-    <div className="App">
-      IT LOGGER APP
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
