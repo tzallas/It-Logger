03b686fc41c31b3f3c336e2f6daf7067da78db0e
tzallas <tzallasilias@gmail.com>
2019-07-24 23:06:27 +0300

    TechListModal.js ready

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 const App = () => {
   useEffect(() => {
@@ -21,6 +23,8 @@ const App = () => {
         <AddBtn />
         <AddLogModal />
         <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
