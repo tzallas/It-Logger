9d2a5f49d936fa6d8c764a76d4efefb5ea56ed96
tzallas <tzallasilias@gmail.com>
2019-08-05 22:51:57 +0300

    ADD_LOG is ready

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
 
 //Get logs from server
@@ -22,7 +22,37 @@ export const getLogs = () => async dispatch => {
     }
 };
 
-//set loading to true
+//Add log
+
+export const addLog = (log) => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch('/logs', {
+            method: "POST",
+            body: JSON.stringify(log),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+};
+
+//Delete log
+
+//Set loading to true
 export const setLoading = () => {
     return {
         type: SET_LOADING
