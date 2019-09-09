17eea42280788241f46634ef52588ef894fbc31b
tzallas <tzallasilias@gmail.com>
2019-09-09 21:45:44 +0300

    Search bar ready

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG, SEARCH_LOGS } from "./types";
 
 
 //Get logs from server
@@ -110,6 +110,26 @@ export const setCurrent = (log) => {
     }
 }
 
+//Search server logs
+export const searchLogs = (text) => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs?q=${text}`);
+        const data = await res.json();
+
+        dispatch({
+            type: SEARCH_LOGS,
+            payload: data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+};
 //Clear current log
 export const clearCurrent = () => {
     return {
