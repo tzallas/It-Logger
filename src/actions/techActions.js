7cd8b9ce103d849203c09f36f6c88cdc8ae186fe
tzallas <tzallasilias@gmail.com>
2019-09-11 00:26:49 +0300

    Add tech is ready

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -21,6 +21,33 @@ export const getTechs = () => async dispatch => {
     }
 };
 
+//Add Technician to server
+export const addTech = (tech) => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch('/techs', {
+            method: 'POST',
+            body: JSON.stringify(tech),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_TECH,
+            payload: data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: TECHS_ERROR,
+            payload: err.response.statusText
+        });
+    }
+};
+
 //Set loading to true
 export const setLoading = () => {
     return {
